82c8723460d162aa1196422eb3a713f5a5a40212
Louis Leclair <56259284+Rolmak06@users.noreply.github.com>
2024-02-05 15:36:08 +0100

    Update DamageableCollider.cs

--- DamageSystem/DamageableCollider.cs
+++ DamageSystem/DamageableCollider.cs
@@ -4,9 +4,13 @@ using System;
 [RequireComponent(typeof(Collider))]
 public class DamageableCollider : MonoBehaviour, IDamageable
 {
+    // Use of an action for the propagation of the hit
     public Action<int> OnHit;
     public int coefficient = 1;
 
+    ///<summary>
+    /// Calls the take damage function from the interface
+    ///</summary>
     public void TakeDamage(int damage)
     {
         OnHit?.Invoke(damage * coefficient);
