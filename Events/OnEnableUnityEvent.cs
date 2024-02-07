bbe834bfb6960d9bde7f9823e2afe4d49d7734e3
Louis Leclair <56259284+Rolmak06@users.noreply.github.com>
2024-02-08 00:09:30 +0100

    Update and rename OnEnabler.cs to OnEnableUnityEvent.cs

--- Events/OnEnableUnityEvent.cs
+++ Events/OnEnableUnityEvent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OnEnabler : MonoBehaviour
+public class OnEnableUnityEvent : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onEnableUnityEvent;
 
