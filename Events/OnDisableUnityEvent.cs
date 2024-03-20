188fa34f2766b1cce8f3a734cb3e2300c2ece840
Louis Leclair <56259284+Rolmak06@users.noreply.github.com>
2024-03-20 11:01:24 +0100

    Update OnDisableUnityEvent.cs
    Fix naming

--- Events/OnDisableUnityEvent.cs
+++ Events/OnDisableUnityEvent.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OnDisableEvent : MonoBehaviour
+public class OnDisableUnityEvent : MonoBehaviour
 {
    public UnityEvent _onDisableUnityEvent;
 
